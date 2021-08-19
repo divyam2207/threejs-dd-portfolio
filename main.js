217e9d33b5e7e48c96fff5b95529644080337170
divyam2207 <61962923+divyam2207@users.noreply.github.com>
2021-08-19 20:45:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import './style.css'
 
 
-import * as THREE from 'three.module.js';
+import * as THREE from '/threejs-dd-portfolio/three.module.js';
 
-import { OrbitControls } from 'OrbitControls.js';
+import { OrbitControls } from '/threejs-dd-portfolio/OrbitControls.js';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
